c51aec6e48d0230729cf73a900271a68918d5443
ganta-karthik <gantakarthikkumar123@gmail.com>
2024-05-16 15:42:40 +0530

    weather-app

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a href="https://ganta-karthik.github.io/portfolio/"
+        > Ganta Karthik Kumar</a
+      >, <a href="https://github.com/shemmee/React-Weather-App">Open sourced on Github.</a> Hosted on Netlify
+    </p>
+  </div>
+  );
+}
